fec2ef64c06e7dad333cdeb0c6624002dbc78997
CodenameCOTC <a.novaladip@yahoo.com>
2019-01-12 22:43:26 +0700

    refactoring to abstract class

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -4,9 +4,19 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+import 'repository.dart';
 
-class NewsDbProvider {
+class NewsDbProvider implements Source, Cache {
   Database db;
+
+  NewsDbProvider() {
+    init();
+  }
+
+  // Todo - store and fetch top ids
+  Future<List<int>> fetchTopIds() {
+    return null;
+  }
 
   init() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
@@ -61,3 +71,5 @@ class NewsDbProvider {
     );
   }
 }
+
+final newsDbProvider = NewsDbProvider();
--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -4,21 +4,46 @@ import 'dart:async';
 import '../models/item_model.dart';
 
 class Repository {
-  NewsDbProvider dbProvider = NewsDbProvider();
-  NewsApiProvider apiProvider = NewsApiProvider();
+  List<Source> sources = <Source>[
+    newsDbProvider,
+    NewsApiProvider(),
+  ];
 
+  List<Cache> caches = <Cache>[
+    newsDbProvider,
+  ];
+
+  // @TODO
+  // Iterate over sources when dbprovider
+  // get fetchTopIds implented
   Future<List<int>> fetchTopIds() {
-    return apiProvider.fetchTopIds();
+    return sources[1].fetchTopIds();
   }
 
   Future<ItemModel> fetchItem(int id) async {
-    var item = await dbProvider.fetchItem(id);
-    if (item != null) {
-      return item;
+    ItemModel item;
+    Source source;
+
+    for (source in sources) {
+      item = await source.fetchItem(id);
+      if (item != null) {
+        break;
+      }
     }
-    item = await apiProvider.fetchItem(id);
-    // inserting item to sqlite
-    dbProvider.addItem(item);
+
+    for (var cache in caches) {
+      cache.addItem(item);
+    }
+
     return item;
   }
+}
+
+abstract class Source {
+  Future<List<int>> fetchTopIds();
+  Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache {
+  Future<int> addItem(ItemModel item);
 }
