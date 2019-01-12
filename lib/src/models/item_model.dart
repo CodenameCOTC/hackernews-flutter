acbc600e0466d0e92910abc055cd35c557e7570e
CodenameCOTC <a.novaladip@yahoo.com>
2019-01-12 21:35:40 +0700

    add: news db provider

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -11,7 +13,7 @@ class ItemModel {
   final String url;
   final int score;
   final String title;
-  final int descendant;
+  final int descendants;
 
   ItemModel.fromJson(Map<String, dynamic> parsedJson)
       : id = parsedJson['id'],
@@ -26,5 +28,37 @@ class ItemModel {
         url = parsedJson['url'],
         score = parsedJson['score'],
         title = parsedJson['title'],
-        descendant = parsedJson['descendant'];
+        descendants = parsedJson['descendants'];
+
+  ItemModel.fromDB(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        deleted = parsedJson['deleted'] == 1,
+        type = parsedJson['type'],
+        by = parsedJson['by'],
+        time = parsedJson['time'],
+        text = parsedJson['text'],
+        dead = parsedJson['dead'] == 1,
+        parent = jsonDecode(parsedJson['parent']),
+        kids = parsedJson['kids'],
+        url = parsedJson['url'],
+        score = parsedJson['score'],
+        title = parsedJson['title'],
+        descendants = parsedJson['descendants'];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "id": id,
+      "type": type,
+      "by": by,
+      "time": time,
+      "parent": parent,
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants,
+      "dead": dead ? 1 : 0,
+      "deleted": deleted ? 1 : 0,
+      "kids": jsonEncode(kids)
+    };
+  }
 }
--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -2,7 +2,6 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:path/path.dart';
-import 'dart:async';
 import '../models/item_model.dart';
 
 class NewsDbProvider {
@@ -36,6 +35,28 @@ class NewsDbProvider {
         )
       """);
       },
+    );
+  }
+
+  fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDB(maps.first);
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel item) {
+    return db.insert(
+      "Items",
+      item.toMap(),
     );
   }
 }
