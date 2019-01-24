770e58618f2182884b0639d53bee13427f44cfb5
CodenameCOTC <a.novaladip@yahoo.com>
2019-01-24 20:22:58 +0700

    add: depth and left padding content

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -5,8 +5,9 @@ import '../models/item_model.dart';
 class Comment extends StatelessWidget {
   final int itemId;
   final Map<int, Future<ItemModel>> itemMap;
+  final int depth;
 
-  Comment({this.itemId, this.itemMap});
+  Comment({this.itemId, this.itemMap, this.depth});
 
   Widget build(context) {
     return FutureBuilder(
@@ -16,8 +17,41 @@ class Comment extends StatelessWidget {
           return Text('Still loading comment');
         }
 
-        return Text(snapshot.data.text);
+        final item = snapshot.data;
+        final children = <Widget>[
+          ListTile(
+            title: buildText(item),
+            subtitle: item.by == "" ? Text("Deleted") : Text(item.by),
+            contentPadding: EdgeInsets.only(
+              right: 16.0,
+              left: (depth + 1) * 16.0,
+            ),
+          ),
+          Divider(),
+        ];
+
+        item.kids.forEach((kidId) {
+          children.add(
+            Comment(
+              itemId: kidId,
+              itemMap: itemMap,
+              depth: depth + 1,
+            ),
+          );
+        });
+
+        return Column(
+          children: children,
+        );
       },
     );
+  }
+
+  Widget buildText(ItemModel item) {
+    final text = item.text
+        .replaceAll('&#x27', "'")
+        .replaceAll('<p>', '\n\n')
+        .replaceAll('</p>', '');
+    return Text(text);
   }
 }
